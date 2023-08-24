b7c334f533ab19d466b36d014d09c030e331eb5b
moon <d.moon23k@gmail.com>
2023-08-24 22:31:02 +0900

    bug fixed

--- model/common.py
+++ model/common.py
@@ -73,24 +73,11 @@ class PositionwiseFeedForward(nn.Module):
 
 
 
-class LayerNorm(nn.Module):
-    def __init__(self, features, eps=1e-6):
-        super(LayerNorm, self).__init__()
-        self.a_2 = nn.Parameter(torch.ones(features))
-        self.b_2 = nn.Parameter(torch.zeros(features))
-        self.eps = eps
-
-    def forward(self, x):
-        mean = x.mean(-1, keepdim=True)
-        std = x.std(-1, keepdim=True)
-        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
-
-
 
 class SublayerConnection(nn.Module):
     def __init__(self, config):
         super(SublayerConnection, self).__init__()
-        self.norm = LayerNorm(config.hidden_dim)
+        self.norm = nn.LayerNorm(config.hidden_dim)
         self.dropout = nn.Dropout(config.dropout_ratio)
 
     def forward(self, x, sublayer):
